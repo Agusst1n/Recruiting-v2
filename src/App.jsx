68513f4b025070900650628e229a3b0d90ea16d8
agusst1n <agustingodoy951@gmail.com>
2022-09-15 05:18:15 -0300

    finish :)

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,11 @@ import React from 'react';
 import './App.css';
 import Container from './components/Container/Container';
 
-// import {BrowserRouter, Routes, Route} from 'react-router-dom'
-// import Register from './pages/Register/Register';
-// import Login from './pages/Login/Login';
-// import { AuthenticationProvider } from './context/AuthenticationContext'
 
 function App() {
   return (
     <div className="App">
-
       <Container/>
-      {/* <BrowserRouter>
-        <AuthenticationProvider>
-          <Routes>
-            <Route path='/home' element={<Container/>}/>
-            <Route path='/register' element={<Register/>}/>
-            <Route path='/' element={<Login/>}/>
-          </Routes>
-        </AuthenticationProvider>
-      </BrowserRouter> */}
     </div>
   );
 }
